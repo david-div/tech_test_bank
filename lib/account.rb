151567b207cfbf4d9b5d04e956c80f0d575d4acd
David Divilly <d.divilly@gmail.com>
2017-10-02 14:23:59 +0100

    added second test and simple cov

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,15 @@
-class Account
+# require './lib/account.rb'
 
+class Account
   attr_reader :deposit
 
   def initialize(deposit)
     @deposit = deposit
+  end
+
+  def statement_amount
+    @total = deposit
+    @total
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,13 +1,18 @@
 require 'account'
 
 RSpec.describe 'Account' do
+
+subject(:account) { Account.new(1000) }
+
   it 'should create a new account' do
     expect(Account).to respond_to :new
   end
 
   it 'should allow you to credit an initial deposit' do
-    account = Account.new(1000)
     expect(account.deposit).to eq(1000)
   end
 
+  it 'should show the statement amount' do
+    expect(account.statement_amount).to eq(1000)
+  end
 end
