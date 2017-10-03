f5538f65d160ae1b440fe3857d7f3b263c081e54
David Divilly <d.divilly@gmail.com>
2017-10-03 12:33:40 +0100

    added print statement to statement class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,1 +1,30 @@
 require 'statement'
+
+RSpec.describe 'Statement' do
+
+  subject(:statement) { Statement.new }
+
+  context '#print_statement' do
+    it 'should print and format the statement' do
+
+      transactions = [
+        {
+          date: Time.now.strftime('%d/%m/%Y'),
+          credit: 1000,
+          debit: 0,
+          balance: 1000
+        },
+        {
+          date: Time.now.strftime('%d/%m/%Y'),
+          credit: 0,
+          debit: 500,
+          balance: 500
+        }
+      ]
+      time = Time.now.strftime('%d/%m/%Y')
+      string = "#{time} || 1000 ||  || 1000\n#{time} || 0 || 500 || 500\n"
+      expect { statement.print_statement(transactions) }.to output(string).to_stdout
+    end
+  end
+
+end
